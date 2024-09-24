25aa8e8a61c2bae3a034355ab642c05e372c2a63
Yvonneizerimana <izerimanayvonne510@gmail.com>
2024-09-24 07:52:50 -0700

    i'm done with login

--- registration/src/app/pages/login/login.component.ts
+++ registration/src/app/pages/login/login.component.ts
@@ -11,5 +11,20 @@ import { RouterModule } from '@angular/router';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  student={
+    email: '',
+    password: ''
+  }
 
+
+studentLogin(){
+  if(this.student.email === 'yvonne@gmail.com' && this.student.password === 'yvonne12345'){
+    alert('Login Successful');
+    console.log(
+      this.student.email,this.student.password
+    );
+  }else{
+    alert('Invalid Credentials');
+  }
+}
 }
