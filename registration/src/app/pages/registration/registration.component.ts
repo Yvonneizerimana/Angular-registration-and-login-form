021e9e645bcefe3d10f8f46b47305000c3f646e6
Yvonneizerimana <izerimanayvonne510@gmail.com>
2024-09-24 07:03:43 -0700

    done with student registration

--- registration/src/app/pages/registration/registration.component.ts
+++ registration/src/app/pages/registration/registration.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
-
+ student={
+  fname:'',
+ lname:'',
+ email:'',
+ password:'',
+ confirmPassword:''
+}
+  studentRegistration(){
+    if(this.student.password !== this.student.confirmPassword){
+   alert('Passwords do not match');
+    }
+    else{
+    console.log("student details",this.student);
+    alert('Registration Successful');
+    }
+  }
 }
